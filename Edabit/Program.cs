f83fa916eb628d341471967a45a6b2f8ce77e2ec
andr942f <56531655+andr942f@users.noreply.github.com>
2020-03-16 08:56:59 +0100

    Initial commit

---
+++ Edabit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Edabit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Very_easy Desc = new Very_easy();
+            Console.WriteLine(Desc.Sum(5, 20));
+            Console.WriteLine(Desc.SameCase("Sup guyS?"));
+            Console.WriteLine(Desc.MissingNum( 1, 2, 3, 4 ));
+        }
+    }
+}
---
+++ Edabit/Very easy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Edabit
+{
+    class Very_easy
+    {
+        /// <summary> Return the Sum of Two Numbers
+        /// <para>Create a function that takes two numbers as arguments and 'return' their sum.</para>
+        /// </summary>
+        public int Sum(int a, int b) => a + b;
+        /// <summary> Check if the Same Case
+        /// <para>Create a function that returns 'true' if an input string contains only uppercase or only lowercase letters.</para>
+        /// </summary>
+        public bool SameCase(string str)
+        {
+            if (str == str.ToLower() || str == str.ToUpper())
+                return true;
+            else return false;
+        }
+        /// <summary> Find the Missing Number
+        /// <para>Create a function that takes an array of numbers between 1 and 10 (excluding one number) and returns the missing number.</para>
+        /// </summary>
+        public static int MissingNum(int[] arr)
+        {
+
+        }
+    }
+}
