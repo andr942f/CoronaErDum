615387763b4c4578ee59ef84649de3880ed535a6
andr942f <56531655+andr942f@users.noreply.github.com>
2020-03-16 13:37:50 +0100

    Fix

--- Opgaver/EdabitOpg/Edabit/Program.cs
+++ Opgaver/EdabitOpg/Edabit/Program.cs
@@ -1,21 +1,12 @@
 ﻿using System;
 
-namespace Edabit
+namespace Opgaver
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Very_easy Desc = new Very_easy();
-            Console.WriteLine(Desc.Sum(5, 20));
-            Console.WriteLine(Desc.SameCase("Sup guyS?"));
-            Console.WriteLine(Desc.LettersOnly("Y¤!#e4#!a¤!#h¤!#t¤#!ha¤4##t¤#4=()/9is¤#42m#¤342e"));
-            Console.WriteLine(Desc.MissingNum(new int[] { 1, 2, 3, 4, 6, 7, 8, 9, 10 }));
-            Console.WriteLine(Desc.match("yo", "Yo"));
-            foreach (var item in Desc.IsFourLetters(new string[] { "Billy", "Illie", "Bill", "Billie" }))
-            {
-                Console.Write(item);
-            }
+            
         }
     }
 }
--- Opgaver/EdabitOpg/Edabit/Very easy.cs
+++ Opgaver/EdabitOpg/Edabit/Very easy.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace Edabit
+namespace Opgaver
 {
     class Very_easy
     {
