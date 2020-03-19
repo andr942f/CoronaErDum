2772d5c71bdbf56bb674ccb39f207915c92f4420
andr942f <56531655+andr942f@users.noreply.github.com>
2020-03-19 14:33:33 +0100

    fix

--- Opgaver/WPF Lommeregner/lommeregner2.0/MainWindow.xaml.cs
+++ Opgaver/WPF Lommeregner/lommeregner2.0/MainWindow.xaml.cs
@@ -42,22 +42,88 @@ namespace lommeregner2._0
         #endregion
 
         #region circumference
-        public Draw draw = new Draw();
-
         // r * r * 3.14
         private void Circle_Click(object sender, RoutedEventArgs e)
         {
-            draw.Cirkel();
+            try
+            {
+                Can.Children.Clear();
+
+                string[] text = Window.Text.Split('*');
+
+                Ellipse ellipse = new Ellipse
+                {
+                    Stroke = Brushes.White,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    Height = int.Parse(text[0] + text[1]),
+                    Width = int.Parse(text[0] + text[1])
+                };
+
+                Can.Children.Add(ellipse);
+            }
+            catch
+            {
+                Window.Text = "Syntax error";
+            }
         }
         // l * b
         private void Square_Click(object sender, RoutedEventArgs e)
         {
-            draw.Cirkel();
+            try
+            {
+                Can.Children.Clear();
+
+                string[] text = Window.Text.Split('*');
+
+                Rectangle rectangle = new Rectangle
+                {
+                    Stroke = Brushes.White,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    Height = int.Parse(text[0]),
+                    Width = int.Parse(text[0])
+
+                };
+
+                Can.Children.Add(rectangle);
+
+            }
+            catch
+            {
+                Window.Text = "Syntax error";
+            }
         }
         // (a + c) * h * 0.5 
         private void Trapezoid_Click(object sender, RoutedEventArgs e)
         {
-            draw.Trapez();
+            try
+            {
+                Can.Children.Clear();
+
+                string[] text = Window.Text.Split('(', ')', '*', '+');
+
+
+                PointCollection points = new PointCollection();
+                points.Add(new Point(0, 0));
+                points.Add(new Point(int.Parse(text[1]), 0));
+                points.Add(new Point(int.Parse(text[2]), int.Parse(text[4])));
+                points.Add(new Point(0.50 * int.Parse(text[4]), int.Parse(text[4])));
+
+                Polygon polygon = new Polygon
+                {
+                    Stroke = Brushes.White,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    Points = points,
+                };
+
+                Can.Children.Add(polygon);
+            }
+            catch
+            {
+                Window.Text = "Syntax error";
+            }
         }
         // h * r * r * 3.14 / 3
         private void Cone_Click(object sender, RoutedEventArgs e)
