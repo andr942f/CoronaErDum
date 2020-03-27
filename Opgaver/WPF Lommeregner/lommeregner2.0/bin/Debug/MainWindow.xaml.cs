5f2f51ecdc15ff95a1ca9558400fd33ea9312293
andr942f <56531655+andr942f@users.noreply.github.com>
2020-03-27 11:31:10 +0100

    Intet nyt, prøvede på noget i min lommeregner, virkede ikke

--- Opgaver/WPF Lommeregner/lommeregner2.0/bin/Debug/MainWindow.xaml.cs
+++ Opgaver/WPF Lommeregner/lommeregner2.0/bin/Debug/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ namespace lommeregner2._0
         // Video brugt som udgangspunkt, har kørt det igennem op til flere gange og har modtaget en forståelse for hvordan det virker: https://www.youtube.com/watch?v=eSrsXt5bP50
         public MainWindow() => InitializeComponent();
         public void Write(string add) => Query.Text += add;
-
+       
         #region B0-9
         private void B0_Click(object sender, RoutedEventArgs e) => Write("0");
         private void B1_Click(object sender, RoutedEventArgs e) => Write("1");
@@ -27,7 +27,7 @@ namespace lommeregner2._0
         private void B9_Click(object sender, RoutedEventArgs e) => Write("9");
         #endregion
         // When clicked, add one of the following (depending on what is clicked) add to textbox
-
+      
         #region Operator chooser
         private void Plus_Click(object sender, RoutedEventArgs e) => Write("+");
         private void Minus_Click(object sender, RoutedEventArgs e) => Write("-");
@@ -37,107 +37,106 @@ namespace lommeregner2._0
         private void Para_Click(object sender, RoutedEventArgs e) => Write("()");
         private void PI_Click(object sender, RoutedEventArgs e) => Write("3.14");
         #endregion
-
+        
         #region Circumference
-        public Circumference circumference = new Circumference();
-
-        /// <summary>
-        /// When clicked, make a cricle using the following 
-        /// <para> equation: r * r * 3.14 </para>
-        /// </summary>
-        private void Circle_Click(object sender, RoutedEventArgs e)
-        {
-            try
+            /// <summary>
+            /// When clicked, make a cricle using the following 
+            /// <para> equation: r * r * 3.14 </para>
+            /// </summary>
+            private void Circle_Click(object sender, RoutedEventArgs e)
             {
-                Can.Children.Clear();
-
-                string[] text = Query.Text.Split('*');
-
-                Ellipse ellipse = new Ellipse
+                try
                 {
-                    Stroke = Brushes.White,
-                    HorizontalAlignment = HorizontalAlignment.Center,
-                    VerticalAlignment = VerticalAlignment.Center,
-                    Height = int.Parse(text[0] + text[1]),
-                    Width = int.Parse(text[0] + text[1])
-                };
+                    Can.Children.Clear();
 
-                Can.Children.Add(ellipse);
-            }
-            catch
-            {
-                Query.Text = "Syntax error";
-            }
-        }
+                    string[] text = Query.Text.Split('*');
 
+                    Ellipse ellipse = new Ellipse
+                    {
+                        Stroke = Brushes.White,
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        Height = int.Parse(text[0] + text[1]),
+                        Width = int.Parse(text[0] + text[1])
+                    };
 
-        /// <summary>
-        /// When clicked, make a cricle using the following 
-        /// <para> equation: l * b </para>
-        /// </summary>
-        private void Square_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                Can.Children.Clear();
-
-                string[] text = Query.Text.Split('*');
-
-                Rectangle rectangle = new Rectangle
+                    Can.Children.Add(ellipse);
+                }
+                catch
                 {
-                    Stroke = Brushes.White,
-                    HorizontalAlignment = HorizontalAlignment.Center,
-                    VerticalAlignment = VerticalAlignment.Center,
-                    Height = int.Parse(text[0]),
-                    Width = int.Parse(text[1])
-
-                };
-
-                Can.Children.Add(rectangle);
-
+                    Query.Text = "Syntax error";
+                }
             }
-            catch
+
+
+            /// <summary>
+            /// When clicked, make a cricle using the following 
+            /// <para> equation: l * b </para>
+            /// </summary>
+            private void Square_Click(object sender, RoutedEventArgs e)
             {
-                Query.Text = "Syntax error";
-            }
-        }
-
-
-        /// <summary>
-        /// When clicked, make a cricle using the following 
-        /// <para> equation: (a + c) * h * 0.5  </para>
-        /// <para> WIP</para>
-        /// </summary>
-        private void Trapezoid_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                Can.Children.Clear();
-
-                string[] text = Query.Text.Split('(', ')', '*', '+');
-
-
-                PointCollection points = new PointCollection();
-                points.Add(new Point(0, 0));
-                points.Add(new Point(int.Parse(text[1]), 0));
-                points.Add(new Point(int.Parse(text[2]), int.Parse(text[4])));
-                points.Add(new Point(0.50 * int.Parse(text[4]), int.Parse(text[4])));
-
-                Polygon polygon = new Polygon
+                try
                 {
-                    Stroke = Brushes.White,
-                    HorizontalAlignment = HorizontalAlignment.Center,
-                    VerticalAlignment = VerticalAlignment.Center,
-                    Points = points,
-                };
+                    Can.Children.Clear();
 
-                Can.Children.Add(polygon);
+                    string[] text = Query.Text.Split('*');
+
+                    Rectangle rectangle = new Rectangle
+                    {
+                        Stroke = Brushes.White,
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        Height = int.Parse(text[0]),
+                        Width = int.Parse(text[1])
+
+                    };
+
+                    Can.Children.Add(rectangle);
+
+                }
+                catch
+                {
+                    Query.Text = "Syntax error";
+                }
             }
-            catch
+
+
+            /// <summary>
+            /// When clicked, make a cricle using the following 
+            /// <para> equation: (a + c) * h * 0.5  </para>
+            /// <para> WIP</para>
+            /// </summary>
+            private void Trapezoid_Click(object sender, RoutedEventArgs e)
             {
-                Query.Text = "Syntax error";
+                try
+                {
+                    Can.Children.Clear();
+
+                    string[] text = Query.Text.Split('(', ')', '*', '+');
+
+
+                    PointCollection points = new PointCollection();
+                    points.Add(new Point(0, 0));
+                    points.Add(new Point(int.Parse(text[1]), 0));
+                    points.Add(new Point(int.Parse(text[2]), int.Parse(text[4])));
+                    points.Add(new Point(0.50 * int.Parse(text[4]), int.Parse(text[4])));
+
+                    Polygon polygon = new Polygon
+                    {
+                        Stroke = Brushes.White,
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        Points = points,
+                    };
+
+                    Can.Children.Add(polygon);
+                }
+                catch
+                {
+                    Query.Text = "Syntax error";
+                }
             }
-        }
+
 
         /// <summary>
         /// When clicked, make a cricle using the following 
@@ -145,48 +144,80 @@ namespace lommeregner2._0
         /// <para> WIP  </para>
         /// </summary>
         private void Cone_Click(object sender, RoutedEventArgs e)
-        {
-            string[] text = Query.Text.Split('*', '/', ' ');
-            circumference.CO(int.Parse(text[0]), int.Parse(text[1]), int.Parse(text[2]));
-        }
-        #endregion
+            {
+                try
+                {
+                    Can.Children.Clear();
 
+                    string[] text = Query.Text.Split('*', '/', ' ');
+                    int h = int.Parse(text[0]);
+                    int r = int.Parse(text[1] + text[2]);
+
+                    Rectangle rectangle = new Rectangle
+                    {
+                        Stroke = Brushes.White,
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        VerticalAlignment = VerticalAlignment.Bottom,
+                        Height = h,
+                        Width = r
+                    };
+
+                    Ellipse ellipse = new Ellipse
+                    {
+                        Stroke = Brushes.White,
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        Height = 10,
+                        Width = r
+                    };
+
+
+                    Can.Children.Add(ellipse);
+                    Can.Children.Add(rectangle);
+                }
+                catch
+                {
+                    Query.Text = "Syntax error";
+                }
+            }
+        #endregion
+       
         #region Delete stuff
-        /// <summary>
-        /// When clicked, Clear Entry 
-        /// <para>Not the best solution, but it works (sort of)</para>
-        /// </summary>
-        private void CE_Click(object sender, RoutedEventArgs e)
-        {
-            Can.Children.Clear();
-            string ce = Query.Text.TrimEnd('*', '/', '+', '-');
-            if (Query.Text.Length != 1)
-                Query.Text = Query.Text.Remove(Query.Text.LastIndexOf(ce) + 1);
+            /// <summary>
+            /// When clicked, Clear Entry 
+            /// <para>Not the best solution, but it works (sort of)</para>
+            /// </summary>
+            private void CE_Click(object sender, RoutedEventArgs e)
+            {
+                Can.Children.Clear();
+                string ce = Query.Text.TrimEnd('*', '/', '+', '-');
+                if (Query.Text.Length != 1)
+                    Query.Text = Query.Text.Remove(Query.Text.LastIndexOf(ce) + 1);
 
-        }
-
-
-        /// <summary>
-        /// When clicked, Clear Equation
-        /// </summary>
-        private void C_Click(object sender, RoutedEventArgs e)
-        {
-            Can.Children.Clear();
-            Query.Text = "";
-            LastQuery.Text = "";
-        }
+            }
 
 
-        /// <summary>
-        /// When clicked, remove 1 from length
-        /// </summary>
-        private void Backspace_Click(object sender, RoutedEventArgs e)
-        {
-            if (Query.Text != "")
-                Query.Text = Query.Text.Remove(Query.Text.Length - 1);
-        }
+            /// <summary>
+            /// When clicked, Clear Equation
+            /// </summary>
+            private void C_Click(object sender, RoutedEventArgs e)
+            {
+                Can.Children.Clear();
+                Query.Text = "";
+                LastQuery.Text = "";
+            }
+
+
+            /// <summary>
+            /// When clicked, remove 1 from length
+            /// </summary>
+            private void Backspace_Click(object sender, RoutedEventArgs e)
+            {
+                if (Query.Text != "")
+                    Query.Text = Query.Text.Remove(Query.Text.Length - 1);
+            }
         #endregion
-
+       
         /// <summary> Equals all numbers together
         /// <para> When clicked, calculate everything in the text window and write the result in the field above</para>
         /// </summary>
