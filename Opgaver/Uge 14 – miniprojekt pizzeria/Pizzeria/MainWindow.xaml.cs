6c2595e2e2d4c348b681e6e44558f0d44ce07f17
andr942f <56531655+andr942f@users.noreply.github.com>
2020-03-30 11:31:02 +0200

    Pizzeria first commit

---
+++ Opgaver/Uge 14 – miniprojekt pizzeria/Pizzeria/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Pizzeria
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Font e = new Font("Ariel", 55);
+        public MainWindow()
+        {
+            InitializeComponent();
+            double[] standard = { 65.00, 80.00, 125.00, 215.00 };
+            Pizza1.Text = Descriptor("Napoli", "Med løg, kylling, dressing og chili", standard);
+            Pizza2.Text = Descriptor("Palermo", "Med kebab, løg, dressing og chili", standard);
+            Pizza3.Text = Descriptor("Catania", "Med skinke og champignon", standard);
+        }
+        public string Descriptor(string navn, string ingredienser, double[] prices) =>
+            $"{navn.ToUpper()}\n" +
+            $"{ingredienser}\n\n" +
+            $"Alm.: {prices[0]} kr.\n" +
+            $"Deep pan: {prices[1]} kr.\n" +
+            $"Fam.: {prices[2]} kr.\n" +
+            $"Mega: {prices[3]} kr.";
+        private void Pizza1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void Pizza2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void Pizza3_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
