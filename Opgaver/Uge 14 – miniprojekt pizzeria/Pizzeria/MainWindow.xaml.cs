28b940aba237f35ac11d9269b915acedf49f41ad
andr942f <56531655+andr942f@users.noreply.github.com>
2020-04-01 09:26:18 +0200

    Pris stuff (har tænk at lave det LIDT bedre at kigge på fremadrettet)

--- Opgaver/Uge 14 – miniprojekt pizzeria/Pizzeria/MainWindow.xaml.cs
+++ Opgaver/Uge 14 – miniprojekt pizzeria/Pizzeria/MainWindow.xaml.cs
@@ -22,18 +22,20 @@ namespace Pizzeria
             $"Deep pan: {prices[1]} kr.\n" +
             $"Fam.: {prices[2]} kr.\n" +
             $"Mega: {prices[3]} kr.";
+        public double[] standard = { 65.00, 80.00, 125.00, 215.00 };
+        public double[] lavSelv= { 0.00, 0.00, 0.00, 0.00 };
         public MainWindow()
         {
             InitializeComponent();
             //Standard prices for pizzas (different pizza types have different prices, i did this so i could take into account if the program should be expanded)
-            double[] standard = { 65.00, 80.00, 125.00, 215.00 };
-
+           
             #region pizza descriptions
             Pizza1.Text = Descriptor("1) Napoli", "Med løg, kylling, dressing og chili", standard);
             Pizza2.Text = Descriptor("2) Palermo", "Med kebab, løg, dressing og chili", standard);
             Pizza3.Text = Descriptor("3) Catania", "Med skinke og champignon", standard);
+
             LavSelv.Text = "4) Lav selv\n".ToUpper() + "Med ";
-            SizeChoose.Text = $"Alm.: { standard[0]} kr.\n" + $"Deep pan: {standard[1]} kr.\n" + $"Fam.: {standard[2]} kr.\n" + $"Mega: {standard[3]} kr.";
+            SizeChoose.Text = $"Alm.: {lavSelv[0]} kr.\n" + $"Deep pan: {lavSelv[1]} kr.\n" + $"Fam.: {lavSelv[2]} kr.\n" + $"Mega: {lavSelv[3]} kr.\n";
             #endregion
         }
 
@@ -41,44 +43,66 @@ namespace Pizzeria
         /// Insert and romove into your self made pizza
         /// </summary>
         /// <param name="item">name of item you want on  your pizza</param>
+        /// /// <param name="alm">price for adding X to alm pizza</param>
+        /// /// <param name="deep">price for adding X to deep pizza</param>
+        /// /// <param name="fam">price for adding X to fam pizza</param>
+        /// /// <param name="mega">price for adding X to mega pizza</param>
         /// <returns></returns>
-        public string Write(string item) => LavSelv.Text.Contains(item) ? LavSelv.Text = LavSelv.Text.Replace(item, "") : LavSelv.Text += item;
-        
+        public string Write(string item, double alm, double deep, double fam, double mega)
+        {
+            if (LavSelv.Text.Contains(item))
+            {
+                lavSelv[0] -= alm;
+                lavSelv[1] -= deep;
+                lavSelv[2] -= fam;
+                lavSelv[3] -= mega;
+            }
+            else
+            {
+                lavSelv[0] += alm;
+                lavSelv[1] += deep;
+                lavSelv[2] += fam;
+                lavSelv[3] += mega;
+            }
+            SizeChoose.Text = $"Alm.: {lavSelv[0]} kr.\n" + $"Deep pan: {lavSelv[1]} kr.\n" + $"Fam.: {lavSelv[2]} kr.\n" + $"Mega: {lavSelv[3]} kr.\n";
+            return LavSelv.Text.Contains(item) ? LavSelv.Text = LavSelv.Text.Replace(item, "") : LavSelv.Text += item;
+        }
+
         // if you want extra on your pizza
         private void Ekstra_Click(object sender, RoutedEventArgs e) => LavSelv.Text += "ekstra ";
 
         #region Toppings
-        private void Løg_Click(object sender, RoutedEventArgs e) => Write("løg, ");
+        private void Løg_Click(object sender, RoutedEventArgs e) => Write($"løg, ", 10, 20, 30, 40);
 
-        private void Ananas_Click(object sender, RoutedEventArgs e) => Write("ananas, ");
+        private void Ananas_Click(object sender, RoutedEventArgs e) => Write("ananas, ", 11, 21, 26, 34);
 
-        private void ChiliPeber_Click(object sender, RoutedEventArgs e) => Write("chili peber, ");
+        private void ChiliPeber_Click(object sender, RoutedEventArgs e) => Write("chili peber, ", 20, 30, 40, 50);
 
-        private void Oliven_Click(object sender, RoutedEventArgs e) => Write("oliven, ");
+        private void Oliven_Click(object sender, RoutedEventArgs e) => Write("oliven, ", 5, 10, 15, 20);
 
-        private void Sovs_Click(object sender, RoutedEventArgs e) => Write("sovs, ");
+        private void Sovs_Click(object sender, RoutedEventArgs e) => Write("sovs, ", 30, 40, 50, 60);
 
-        private void Ost_Click(object sender, RoutedEventArgs e) => Write("ost, ");
+        private void Ost_Click(object sender, RoutedEventArgs e) => Write("ost, ", 30, 40, 50, 60);
         #endregion
 
         #region Crust and dough
-        private void Indbagt_Click(object sender, RoutedEventArgs e) => Write("indbagt, ");
+        private void Indbagt_Click(object sender, RoutedEventArgs e) => Write("indbagt, ", 32, 42, 52, 62);
 
-        private void Normal_Click(object sender, RoutedEventArgs e) => Write("normal, ");
+        private void Normal_Click(object sender, RoutedEventArgs e) => Write("normal, ", 30, 40, 50, 60);
         #endregion
 
         #region ingridents
-        private void Paprika_Click(object sender, RoutedEventArgs e) => Write("paprika, ");
+        private void Paprika_Click(object sender, RoutedEventArgs e) => Write("paprika, ", 4, 8, 12, 16);
 
-        private void Pizzakrydder_Click(object sender, RoutedEventArgs e) => Write("pizzakrydder, ");
+        private void Pizzakrydder_Click(object sender, RoutedEventArgs e) => Write("pizzakrydder, ", 2, 4, 6, 8);
         #endregion
 
         #region choose other pizzas to modify
-        private void one1_Click(object sender, RoutedEventArgs e) => Write("Napoli, ");
+        private void one1_Click(object sender, RoutedEventArgs e) => Write("Napoli, ", standard[0], standard[1], standard[2], standard[3]);
 
-        private void two2_Click(object sender, RoutedEventArgs e) => Write("Palermo, ");
+        private void two2_Click(object sender, RoutedEventArgs e) => Write("Palermo, ", standard[0], standard[1], standard[2], standard[3]);
 
-        private void three3_Click(object sender, RoutedEventArgs e) => Write("Catania, ");
+        private void three3_Click(object sender, RoutedEventArgs e) => Write("Catania, ", standard[0], standard[1], standard[2], standard[3]);
         #endregion
 
         #region Choose size of pizza
