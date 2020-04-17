5bdb3d98de40e7f999249ec6334556dbdc67c1b7
andr942f <56531655+andr942f@users.noreply.github.com>
2020-04-17 12:01:02 +0200

    RegEx
    Shit is getting wild

--- Opgaver/RegEx/RegEx/Program.cs
+++ Opgaver/RegEx/RegEx/Program.cs
@@ -8,25 +8,42 @@ namespace RegEx
         //https://www.youtube.com/watch?v=sa-TUpSx1JA
         //https://www.youtube.com/watch?v=9178EdR6Pyg
         //https://www.regextester.com/97589 Used to test RegEx
+        //https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/xmldoc/summary Been working on this most of 4/17/2020, mostly to understand it, and use it in the future
 
-        public static string str =
-        /*reg1 */ "Acorn Pcorn pcorn acorn ecor whoops " +
-        /*reg2 */ "Mr. Gman Mrs. Gman Mr Billy Mr bob asc " +
-        /*reg3 */ "andreasvbeck420@hotmail.com ad@hot.dk realg@gmail.dk bruh@gmail.bruh a@e.co" +
-        /*reg4 */ "[stop] puting me in [brackets] please";
+        /// <summary>
+        /// A string for all of the various RegEx's
+        /// </summary>
+        public static string[] str =
+        { 
+            /*reg1 */ "Acorn Pcorn pcorn acorn ecor whoops",
+            /*reg2 */ "Mr. Gman Mrs. Gman Mr Billy Mr bob asc",
+            /*reg3 */ "andreasvbeck420@hotmail.com ad@hot.dk realg@gmail.dk bruh@gmail.bruh a@e.co",
+            /*reg4 */ "[stop] puting me in [brackets] please",
+            /*reg5 */ "Password1234 password123 Password",
+            /*reg6 */ "59001 853a7 732 32 393939",
+            /*reg7 */ "1111"
+        };
 
+        /// <summary>
+        /// Instead of writing the same thing a billion times
+        /// </summary>
+        /// <param name="matches"></param>
         public static void ForLoop(MatchCollection matches)
         {
             foreach (Match match in matches)
                 Console.Write(match + ", ");
             Console.WriteLine();
         }
+
         public static void Main()
         {
-            ForLoop(RegEx.reg1.Matches(str));
-            ForLoop(RegEx.reg2.Matches(str));
-            ForLoop(RegEx.reg3.Matches(str));
-            ForLoop(RegEx.reg4.Matches(str));
+            ForLoop(RegEx.reg1.Matches(str[0]));
+            ForLoop(RegEx.reg2.Matches(str[1]));
+            ForLoop(RegEx.reg3.Matches(str[2]));
+            ForLoop(RegEx.reg4.Matches(str[3]));
+            ForLoop(RegEx.reg5.Matches(str[4]));
+            ForLoop(RegEx.reg6.Matches(str[5]));
+            ForLoop(RegEx.reg7.Matches(str[6]));
         }
     }
 }
--- Opgaver/RegEx/RegEx/RegEx.cs
+++ Opgaver/RegEx/RegEx/RegEx.cs
@@ -7,9 +7,39 @@ namespace RegEx
 {
     public class RegEx
     {
+        /// <summary>
+        /// Any corn but acorns!
+        /// </summary>
         public static Regex reg1 = new Regex(@"[^Aa]corn");
+        /// <summary>
+        /// mr or mrs. x 
+        /// </summary>
         public static Regex reg2 = new Regex(@"M(r|s|rs)\.?\s[a-zA-Z]\w*");
+        /// <summary>
+        /// Is it a valid email?
+        /// </summary>
         public static Regex reg3 = new Regex(@"\w*@\b(hotmail|gmail)\.\b(com|dk)");
+        /// <summary>
+        /// is it in [brackets]?
+        /// </summary>
         public static Regex reg4 = new Regex(@"\[\w*\]");
+        /// <summary>
+        /// Is it a valid password?
+        /// </summary>
+        public static Regex reg5 = new Regex(@"[A-Z]\w*[0-9]");
+        /// <summary>
+        /// Edabit opg: Valid Zip Code (regex edition)
+        /// </summary>
+        public static Regex reg6 = new Regex(@"\b\d{5}\b");
+        /// <summary>
+        /// Find NON prime numbers
+        /// <para>Input is in unary form. 1 is 1, 2 is 11, 3 is 111, etc. Zero is an empty string.</para>
+        /// <para>The first part of the regex matches 0 and 1 as non-prime.The second is where the magic kicks in.</para>
+        /// <para>(11+?) starts by finding divisors.It starts by being defined as 11, or 2. \1 is a variable referring to that previously captured match, so \1+ determines if the number is divisible by that divisor. (111111 starts by assigning the variable to 11, and then determines that the remaining 1111 is 11 repeated, so 6 is divisible by 2.)</para>
+        /// <para>If the number is not divisible by two, the regex engine increments the divisor. (11+?) becomes 111, and we try again.If at any point the regex matches, the number has a divisor that yields no remainder, and so the number cannot be prime.</para>
+        /// </summary>
+        public static Regex reg7 = new Regex(@"^1?$|^(11+?)\1+$");
+        
+        
     }
 }
