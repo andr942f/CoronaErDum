49763d648b498241cce9e279eb8c3e21c6428b47
andr942f <56531655+andr942f@users.noreply.github.com>
2020-04-20 12:28:34 +0200

    RegexOne opgaver

--- Opgaver/RegEx/RegEx/Program.cs
+++ Opgaver/RegEx/RegEx/Program.cs
@@ -9,41 +9,105 @@ namespace RegEx
         //https://www.youtube.com/watch?v=9178EdR6Pyg
         //https://www.regextester.com/97589 Used to test RegEx
         //https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/xmldoc/summary Been working on this most of 4/17/2020, mostly to understand it, and use it in the future
+        //Exersises: https://regexone.com/
 
         /// <summary>
         /// A string for all of the various RegEx's
         /// </summary>
         public static string[] str =
         { 
-            /*reg1 */ "Acorn Pcorn pcorn acorn ecor whoops",
-            /*reg2 */ "Mr. Gman Mrs. Gman Mr Billy Mr bob asc",
-            /*reg3 */ "andreasvbeck420@hotmail.com ad@hot.dk realg@gmail.dk bruh@gmail.bruh a@e.co",
-            /*reg4 */ "[stop] puting me in [brackets] please",
-            /*reg5 */ "Password1234 password123 Password",
-            /*reg6 */ "59001 853a7 732 32 393939",
-            /*reg7 */ "1111"
+            /*reg0 */ "Acorn Pcorn pcorn acorn ecor whoops",
+            /*reg1 */ "Mr. Gman Mrs. Gman Mr Billy Mr bob asc",
+            /*reg2 */ "andreasvbeck420@hotmail.com ad@hot.dk realg@gmail.dk bruh@gmail.bruh a@e.co",
+            /*reg3 */ "[stop] puting me in [brackets] please",
+            /*reg4 */ "Password1234 password123 Password",
+            /*reg5 */ "59001 853a7 732 32 393939",
+            /*reg6 */ "1111",
+
+            // RegexOne opgaver
+                /*reg7 */ "whssup",
+                /*reg8 */ "1111asvssa",
+                /*reg9 */ "1111.sas.",
+                /*reg10 */ "nan",
+                /*reg11 */ "pog",
+                /*reg12 */ "AASDsaasDaasdASD",
+                /*reg13 */ "Wazzzzzzup",
+                /*reg14 */ "aaaaaacc",
+                /*reg15 */ "cc",
+                /*reg16 */ "Wha    t",
+                /*reg17 */ "Mission: successful",
+                /*reg18 */ "file_07241999.pdf",
+                /*reg19 */ "Jan 1987",
+                /*reg20 */ "1920x1600",
+                /*reg21 */ "I love cats",
+                /*reg22 */ "Andreas is cool",
+                /*reg23 */ "123,340.00",
+                /*reg24 */ "(416)555-3456",
+                /*reg25 */ "Andreas.vbeck@hotmail.com",
+                /*reg26 */ "<a>This is a link</a>",
+                /*reg27 */ "updated_img0912.png",
+                /*reg28 */ "				The quick brown fox...",
+                /*reg29 */ "E/( 1553):   at widget.List.fillFrom(ListView.java:709)",
+                /*reg30 */ "ftp://file_server.com:21/top_secret/life_changing_plans.pdf",
+            // end
         };
 
         /// <summary>
         /// Instead of writing the same thing a billion times
         /// </summary>
         /// <param name="matches"></param>
-        public static void ForLoop(MatchCollection matches)
+        public static void ForLoop(MatchCollection matches, bool length)
         {
-            foreach (Match match in matches)
-                Console.Write(match + ", ");
-            Console.WriteLine();
+            switch (length)
+            {
+                case true:
+                    foreach (Match match in matches)
+                        Console.Write(match.Length.ToString() + ", ");
+                    Console.WriteLine();
+                    break;
+                default:
+                    foreach (Match match in matches)
+                        Console.Write(match + ", ");
+                    Console.WriteLine();
+                    break;
+            }
         }
 
         public static void Main()
         {
-            ForLoop(RegEx.reg1.Matches(str[0]));
-            ForLoop(RegEx.reg2.Matches(str[1]));
-            ForLoop(RegEx.reg3.Matches(str[2]));
-            ForLoop(RegEx.reg4.Matches(str[3]));
-            ForLoop(RegEx.reg5.Matches(str[4]));
-            ForLoop(RegEx.reg6.Matches(str[5]));
-            ForLoop(RegEx.reg7.Matches(str[6]));
+            ForLoop(RegEx.reg0.Matches(str[0]), false);
+            ForLoop(RegEx.reg1.Matches(str[1]), false);
+            ForLoop(RegEx.reg2.Matches(str[2]), false);
+            ForLoop(RegEx.reg3.Matches(str[3]), false);
+            ForLoop(RegEx.reg4.Matches(str[4]), false);
+            ForLoop(RegEx.reg5.Matches(str[5]), false);
+            ForLoop(RegEx.reg6.Matches(str[6]), true);
+
+            //RegexOne opgaver
+                ForLoop(RegEx.reg7.Matches(str[7]), false);
+                ForLoop(RegEx.reg8.Matches(str[8]), false);
+                ForLoop(RegEx.reg9.Matches(str[9]), false);
+                ForLoop(RegEx.reg10.Matches(str[10]), false);
+                ForLoop(RegEx.reg11.Matches(str[11]), false);
+                ForLoop(RegEx.reg12.Matches(str[12]), false);
+                ForLoop(RegEx.reg13.Matches(str[13]), false);
+                ForLoop(RegEx.reg14.Matches(str[14]), false);
+                ForLoop(RegEx.reg15.Matches(str[15]), false);
+                ForLoop(RegEx.reg16.Matches(str[16]), false);
+                ForLoop(RegEx.reg17.Matches(str[17]), false);
+                ForLoop(RegEx.reg18.Matches(str[18]), false);
+                ForLoop(RegEx.reg19.Matches(str[19]), false);
+                ForLoop(RegEx.reg20.Matches(str[20]), false);
+                ForLoop(RegEx.reg21.Matches(str[21]), false);
+                ForLoop(RegEx.reg22.Matches(str[22]), false);
+                ForLoop(RegEx.reg23.Matches(str[23]), false);
+                ForLoop(RegEx.reg24.Matches(str[24]), false);
+                ForLoop(RegEx.reg26.Matches(str[26]), false);
+                ForLoop(RegEx.reg27.Matches(str[27]), false);
+                ForLoop(RegEx.reg28.Matches(str[28]), false);
+                ForLoop(RegEx.reg29.Matches(str[29]), false);
+                ForLoop(RegEx.reg30.Matches(str[30]), false);
+            //end
         }
     }
 }
--- Opgaver/RegEx/RegEx/RegEx.cs
+++ Opgaver/RegEx/RegEx/RegEx.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace RegEx
 {
@@ -10,27 +7,27 @@ namespace RegEx
         /// <summary>
         /// Any corn but acorns!
         /// </summary>
-        public static Regex reg1 = new Regex(@"[^Aa]corn");
+        public static Regex reg0 = new Regex(@"[^Aa]corn");
         /// <summary>
         /// mr or mrs. x 
         /// </summary>
-        public static Regex reg2 = new Regex(@"M(r|s|rs)\.?\s[a-zA-Z]\w*");
+        public static Regex reg1 = new Regex(@"M(r|s|rs)\.?\s[a-zA-Z]\w*");
         /// <summary>
         /// Is it a valid email?
         /// </summary>
-        public static Regex reg3 = new Regex(@"\w*@\b(hotmail|gmail)\.\b(com|dk)");
+        public static Regex reg2 = new Regex(@"\w*@\b(hotmail|gmail)\.\b(com|dk)");
         /// <summary>
         /// is it in [brackets]?
         /// </summary>
-        public static Regex reg4 = new Regex(@"\[\w*\]");
+        public static Regex reg3 = new Regex(@"\[\w*\]");
         /// <summary>
         /// Is it a valid password?
         /// </summary>
-        public static Regex reg5 = new Regex(@"[A-Z]\w*[0-9]");
+        public static Regex reg4 = new Regex(@"[A-Z]\w*[0-9]");
         /// <summary>
         /// Edabit opg: Valid Zip Code (regex edition)
         /// </summary>
-        public static Regex reg6 = new Regex(@"\b\d{5}\b");
+        public static Regex reg5 = new Regex(@"\b\d{5}\b");
         /// <summary>
         /// Find NON prime numbers
         /// <para>Input is in unary form. 1 is 1, 2 is 11, 3 is 111, etc. Zero is an empty string.</para>
@@ -38,8 +35,106 @@ namespace RegEx
         /// <para>(11+?) starts by finding divisors.It starts by being defined as 11, or 2. \1 is a variable referring to that previously captured match, so \1+ determines if the number is divisible by that divisor. (111111 starts by assigning the variable to 11, and then determines that the remaining 1111 is 11 repeated, so 6 is divisible by 2.)</para>
         /// <para>If the number is not divisible by two, the regex engine increments the divisor. (11+?) becomes 111, and we try again.If at any point the regex matches, the number has a divisor that yields no remainder, and so the number cannot be prime.</para>
         /// </summary>
-        public static Regex reg7 = new Regex(@"^1?$|^(11+?)\1+$");
-        
-        
+        public static Regex reg6 = new Regex(@"^1?$|^(11+?)\1+$");
+
+        //RegexOme opgaver
+            /// <summary>
+            /// Lesson 1: An Introduction, and the ABCs
+            /// </summary>
+            public static Regex reg7 = new Regex(@"[a-z]");
+            /// <summary>
+            /// Lesson 1½: The 123s
+            /// </summary>
+            public static Regex reg8 = new Regex(@"\d");
+            /// <summary>
+            /// Lesson 2: The Dot
+            /// </summary>
+            public static Regex reg9 = new Regex(@"\.");
+            /// <summary>
+            /// Lesson 3: Matching specific characters
+            /// </summary>
+            public static Regex reg10 = new Regex(@"[^drp]an");
+            /// <summary>
+            /// Lesson 4: Excluding specific characters
+            /// </summary>
+            public static Regex reg11 = new Regex(@"[^b]og");
+            /// <summary>
+            /// Lesson 5: Character ranges
+            /// </summary>
+            public static Regex reg12 = new Regex(@"[A-Z]");
+            /// <summary>
+            /// Lesson 6: Catching some zzz's
+            /// </summary>
+            public static Regex reg13 = new Regex(@"wa{3,11}up");
+            /// <summary>
+            /// Lesson 7: Mr. Kleene, Mr. Kleene
+            /// </summary>
+            public static Regex reg14 = new Regex(@"a+b*c+");
+            /// <summary>
+            /// Lesson 8: Characters optional
+            /// </summary>
+            public static Regex reg15 = new Regex(@"\?");
+            /// <summary>
+            /// Lesson 9: All this whitespace
+            /// </summary>
+            public static Regex reg16 = new Regex(@"\s");
+            /// <summary>
+            /// Lesson 10: Starting and ending
+            /// </summary>
+            public static Regex reg17 = new Regex(@"[^un]successful$");
+            /// <summary>
+            /// Lesson 11: Match groups
+            /// </summary>
+            public static Regex reg18 = new Regex(@"(file_(\w*))\.pdf$");
+            /// <summary>
+            /// Lesson 12: Nested groups
+            /// </summary>
+            public static Regex reg19 = new Regex(@"(\w* (\d*))");
+            /// <summary>
+            /// Lesson 13: More group work
+            /// </summary>
+            public static Regex reg20 = new Regex(@"(\d*)x(\d*)");
+            /// <summary>
+            /// Lesson 14: It's all conditional
+            /// </summary>
+            public static Regex reg21 = new Regex(@"(cats|dogs)");
+            /// <summary>
+            /// Lesson 15: Other special characters
+            /// </summary>
+            public static Regex reg22 = new Regex(@"\w\s");
+            /// <summary>
+            /// Problem 1: Matching a decimal numbers
+            /// </summary>
+            public static Regex reg23 = new Regex(@"\d$");
+            /// <summary>
+            /// Problem 2: Matching phone numbers
+            /// </summary>
+            public static Regex reg24 = new Regex(@"(\d{3})");
+            /// <summary>
+            /// Problem 3: Matching emails
+            /// </summary>
+            public static Regex reg25 = new Regex(@"^([\w\.]*)");
+            /// <summary>
+            /// Problem 4: Matching HTML
+            /// </summary>
+            public static Regex reg26 = new Regex(@"<(\w{0,3})");
+            /// <summary>
+            /// Problem 5: Matching specific filenames
+            /// </summary>
+            public static Regex reg27 = new Regex(@"(\w*)\.(jpg|png|gif)$");
+            /// <summary>
+            /// Problem 6: Trimming whitespace from start and end of line
+            /// </summary>
+            public static Regex reg28 = new Regex(@"\s*(.*)\s*");
+            /// <summary>
+            /// Problem 7: Extracting information from a log file
+            /// </summary>
+            public static Regex reg29 = new Regex(@"(\w*)\(([\w\.]*):(\d*)\)");
+            /// <summary>
+            /// Problem 8: Parsing and extracting data from a URL
+            /// </summary>
+            public static Regex reg30 = new Regex(@"(\w*)://([\w\-\.]*)(:([\d]*))*");
+        //end
+
     }
 }
