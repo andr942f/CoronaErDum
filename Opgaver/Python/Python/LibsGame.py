9d327702e21ad7307a5d9dc89cac9cd133d58744
andr942f <56531655+andr942f@users.noreply.github.com>
2020-05-11 14:37:56 +0200

    Python

---
+++ Opgaver/Python/Python/LibsGame.py
@@ -0,0 +1,13 @@
+class LibsGame(object):
+    """description of class"""
+    gender = input("enter a gender: ")
+    adjective = input("enter a adjective: ")
+    verb1 = input("enter a verb: ")
+    verb2 = input("enter a verb: ")
+
+
+    print(f"There once was a {gender}, {gender} was {adjective}")
+    print(f"{gender} was so {adjective} in fact, that he was {verb1} by his peers")
+    print(f"{gender} then {verb2}, the end")
+
+
---
+++ Opgaver/Python/Python/Main.py
@@ -0,0 +1,15 @@
+#Youtube help https://www.youtube.com/watch?v=rfscVS0vtbw&t=545s
+class Main:
+    print("Hello world")
+
+    int = 0
+
+
+    while int <= 10:
+        print(int)
+        int += 1
+
+from LibsGame import LibsGame
+
+
+
