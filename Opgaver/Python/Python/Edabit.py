7d3f0a61dde9344d92cce6ee7dd0b76fc6c0f9e9
andr942f <56531655+andr942f@users.noreply.github.com>
2020-05-14 14:37:26 +0200

    Python

---
+++ Opgaver/Python/Python/Edabit.py
@@ -0,0 +1,80 @@
+class Edabit(object):
+    """description of class"""
+
+    def hello():
+        return "hello edabit.com"
+
+    def addition(a, b):
+        return a + b
+
+    def next_edge(side1, side2):
+        return (side1 + side2) - 1
+
+    def get_first_value(number_list):
+        return number_list[0]
+
+    def calculate_exponent(num, exp):
+        return num**exp
+
+    def how_many_seconds(hours):
+        return (hours * 60) * 60
+
+    def find_perimeter(height, width):
+        return 2 * (height + width)
+
+    def convert(minutes):
+        return minutes * 60
+
+    def And(a, b):
+        return a and b
+
+    def to_int(txt):
+        return int(txt)
+
+    def to_str(num):
+        return str(num)
+
+    def animals(chickens, cows, pigs):
+        return (chickens * 2) + (cows * 4) + (pigs * 4)
+
+    def get_sum_of_elements(lst):
+        return sum(lst)
+
+    def concat(lst1, lst2):
+        return lst1 + lst2
+
+    def addition2(num):
+        return num + 1
+
+    def difference_max_min(lst):
+        return max(lst) - min(lst)
+
+    def convertsec(hours, minutes):
+        return ((hours * 60 + minutes) * 60)
+
+    def is_same_num(num1, num2):
+        return num1 == num2
+
+    def findLargestNum(nums):
+        return max(nums)
+
+    def remainder(x, y):
+        return x % y
+
+    def divisible(num):
+        return num % 100 == 0
+
+    def less_than_100(a, b):
+        return a + b < 100
+
+    def less_than_or_equal_to_zero(num):
+        return num <= 0
+
+    def squared(b):
+        return b * b
+
+    def tri_area(base, height):
+        return (base * height) / 2
+
+    def comp(txt1, txt2):
+        return len(txt1) == len(txt2)
--- Opgaver/Python/Python/Main.py
+++ Opgaver/Python/Python/Main.py
@@ -1,16 +1,42 @@
 #Youtube help https://www.youtube.com/watch?v=rfscVS0vtbw&t=545s
+
+from Edabit import Edabit
+print(Edabit.hello())
+print(Edabit.addition(5, 5))
+print(Edabit.next_edge(5, 5))
+print(Edabit.get_first_value([1, 2, 3, 4, 5]))
+print(Edabit.calculate_exponent(5, 2))
+print(Edabit.how_many_seconds(2))
+print(Edabit.find_perimeter(17, 17))
+print(Edabit.convert(2))
+print(Edabit.And(False, True))
+print(Edabit.to_int("5"))
+print(Edabit.to_str(5))
+print(Edabit.animals(5, 6, 2))
+print(Edabit.get_sum_of_elements([10, 60, 70, 21, 5]))
+print(Edabit.concat([1, 2, 3, 4, 5], [10, 60, 70, 21, 5]))
+print(Edabit.addition2(5))
+print(Edabit.difference_max_min([1, 5, 10, 15, 20]))
+print(Edabit.convertsec(5, 10))
+print(Edabit.is_same_num(5, 10))
+print(Edabit.findLargestNum([5, 10, 100, 2, 60]))
+print(Edabit.remainder(5, 10))
+print(Edabit.divisible(5))
+print(Edabit.less_than_100(5, 10))
+print(Edabit.less_than_or_equal_to_zero(5))
+print(Edabit.squared(5))
+print(Edabit.tri_area(5, 10))
+print(Edabit.comp("aaa", "aa"))
+
+
+print("")
 class Main:
     print("Hello world")
-
     int = 0
-
-
     while int <= 10:
         print(int)
         int += 1
-
 from LibsGame import LibsGame
 from GuessingGame import Guessing_game
-
 
 
