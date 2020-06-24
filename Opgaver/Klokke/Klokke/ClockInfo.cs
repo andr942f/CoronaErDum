35ad2529cf82eb198cc09da965e9656b5bf0fddc
andr942f <56531655+andr942f@users.noreply.github.com>
2020-06-24 13:25:17 +0200

    Klokke

---
+++ Opgaver/Klokke/Klokke/ClockInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Klokke
+{
+    public class ClockInfo
+    {
+        
+        public static DispatcherTimer Time = new DispatcherTimer();
+        public static readonly Stopwatch StopWatch = new Stopwatch();
+        public static readonly Stopwatch Lap = new Stopwatch();
+        public static readonly Stopwatch CountDown = new Stopwatch();
+        public static string Format => "HH:mm:ss";
+    }
+}
---
+++ Opgaver/Klokke/Klokke/MainWindow.xaml.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace Klokke
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //TimeDisplay.Text = Display.TimeNow();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        TimeSpan t;
+        private void LiveTime_Tick(object sender, EventArgs e) => TimeDisplay.Content = Display.TimeNow();
+        private void LiveTime_Tick2(object sender, EventArgs e) => Watch.Content = Display.TimeNow();
+
+
+        /// <summary>
+        /// Counter
+        /// </summary>
+        public void Updater(EventHandler e)
+        {
+            ClockInfo.Time.Tick += e;
+            ClockInfo.Time.Start();
+        }
+
+        /// <summary>
+        /// Updater and timer controller
+        /// </summary>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            EditList.ItemsSource = WatchList.Items;
+            ClockInfo.Time.Interval = TimeSpan.FromSeconds(1);
+            Updater(LiveTime_Tick);
+            Updater(LiveTime_Tick2);
+            Updater(CountDownTimer);
+            Updater(AlarmBib);
+
+        }
+
+        #region Stopwatch Code
+        TimeSpan lap;
+        /// <summary>
+        /// Display Time
+        /// </summary>
+        public void Timer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan sw = ClockInfo.StopWatch.Elapsed;
+            lap = ClockInfo.Lap.Elapsed;
+            StopWatchDisplay.Content = string.Format("{0:00}:{1:00}:{2:00}", sw.Hours, sw.Minutes, sw.Seconds);
+            LapDisplay.Content = string.Format("{0:00}:{1:00}:{2:00}", lap.Hours, lap.Minutes, lap.Seconds);
+        }
+        private void Lap_Click(object sender, RoutedEventArgs e)
+        {
+            ClockInfo.Lap.Restart();
+            LapTimer.Items.Add(LapDisplay.Content);
+        }
+        /// <summary>
+        /// Start timer
+        /// </summary>
+        private void StopwatchStart_Click(object sender, RoutedEventArgs e)
+        {
+            ClockInfo.StopWatch.Start();
+            ClockInfo.Lap.Start();
+            Updater(Timer_Tick);
+        }
+        /// <summary>
+        /// Stop timer
+        /// </summary>
+        private void StopwatchStop_Click(object sender, RoutedEventArgs e)
+        {
+            LapTimer.Items.Clear();
+            ClockInfo.StopWatch.Stop();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StopwatchReset_Click(object sender, RoutedEventArgs e) => ClockInfo.StopWatch.Restart();
+        #endregion
+
+        #region Countdown Code
+        /// <summary>
+        /// Counts down each second, and makes sure there are no letters in the textboxes
+        /// </summary>
+        TimeSpan form;
+        List<string> list = new List<string>();
+        private void CountDownTimer(object sender, EventArgs e)
+        {
+            form = t - ClockInfo.CountDown.Elapsed;
+            if (form.Hours < 0 && form.Minutes < 0 && form.Seconds < 0)
+            {
+                ClockInfo.CountDown.Stop();
+                CountDown.Content = string.Format("{0:00}:{1:00}:{2:00}", 0, 0, 0);
+            }
+            if (Regex.IsMatch(Hours.Text, "[^0-9]") || Regex.IsMatch(Minutes.Text, "[^0-9]") || Regex.IsMatch(Seconds.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please enter only numbers.");
+                Hours.Text = "0";
+                Minutes.Text = "0";
+                Seconds.Text = "0";
+            }
+            else
+            {
+                
+            }
+        }
+    }
+    private void AddTimer_Click(object sender, RoutedEventArgs e)
+    {
+        string timeSpan = string.Format("{0:00}:{1:00}:{2:00}", form.Hours, form.Minutes, form.Seconds);
+        list.Add(timeSpan);
+        CountDownList.Items.Add(list);
+    }
+    private void StartCountdown_Click(object sender, RoutedEventArgs e) => ClockInfo.CountDown.Start();
+    private void PlusCountdown_Click(object sender, RoutedEventArgs e) => t += new TimeSpan(Convert.ToInt32(Hours.Text), Convert.ToInt32(Minutes.Text), Convert.ToInt32(Seconds.Text));
+    private void MinusCountdown_Click(object sender, RoutedEventArgs e) => t -= new TimeSpan(Convert.ToInt32(Hours.Text), Convert.ToInt32(Minutes.Text), Convert.ToInt32(Seconds.Text));
+    private void StopCountdown_Click(object sender, RoutedEventArgs e) => ClockInfo.CountDown.Stop();
+    private void ResetCountdown_Click(object sender, RoutedEventArgs e)
+    {
+        ClockInfo.CountDown.Stop();
+        CountDown.Content = string.Format("{0:00}:{1:00}:{2:00}", 0, 0, 0);
+    }
+    #endregion
+
+    #region Watch code
+    /// <summary>
+    /// When alarm is done, let me know
+    /// </summary>
+    private void AlarmBib(object sender, EventArgs e)
+    {
+        if (WatchList.Items.Contains($"{DateTime.Now.DayOfWeek.ToString()} - {Display.TimeNow()}"))
+            MessageBox.Show(SelvText.Text);
+
+    }
+    private void WatchAdd_Click(object sender, RoutedEventArgs e)
+    {
+        WatchList.Items.Add($"{DayOfWeek.Text} - {Hours1.Text}:{Minutes1.Text}:{Seconds1.Text} ({SelvText.Text})");
+    }
+    private void Edit_Click(object sender, RoutedEventArgs e)
+    {
+        WatchList.Items.Remove(EditList.Text);
+        WatchList.Items.Add(EditList.Text = $"{DayOfWeek.Text} - {Hours1.Text}:{Minutes1.Text}:{Seconds1.Text} ({SelvText.Text})");
+    }
+    #endregion
+
+}
+}
